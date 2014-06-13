9479788dfa5af4b391f93fa2a90673e6bfeb4fc0
neekras <nick.hampshire93@gmail.com>
2014-06-14 01:39:54 +1000

    Updated header comment

--- src/GBBMain.java
+++ src/GBBMain.java
@@ -1,3 +1,10 @@
+/* GBBMain.java
+ * Author: Nick Hampshire, using pircbot framework
+ * Website: nhampshire.com
+ * Description:
+ * Starts up bot and connects to channel.
+ */
+
 import org.jibble.pircbot.*;
 import java.io.*;
 
@@ -5,7 +12,7 @@ public class GBBMain {
     
     public static void main(String[] args) throws Exception {
         
-        // Now start our bot up.
+        // Start up bot
         System.out.print("Starting bot...");
         GBB9000 bot = new GBB9000();
         System.out.print("Done! \n");
@@ -13,12 +20,12 @@ public class GBBMain {
         // Enable debugging output.
         bot.setVerbose(false);
         
-        // Connect to the IRC server.
+        // Connect to the twitch IRC server.
         System.out.print("Connecting to Server...");
         bot.connect("irc.twitch.tv", 6667, "oauth:XXXXXXXXXXX"); //change oauth to the one for your twitch channel
         System.out.print("Done! \n");
 
-        // Join the #pircbot channel.
+        // Join the twitch channel.
         System.out.print("Joining Channel...");
         bot.joinChannel("#username");  //change to your twitch username
         System.out.print("Done! \n");
